05c9d6b646796e7b0504cb32ad57a434abceae49
Martin Hallbäck <marha614@student.liu.se>
2024-11-19 10:01:23 +0100

    started on lab1_3

--- lab1_3.R
+++ lab1_3.R
@@ -1,1 +1,7 @@
 #marha614, wiler441, emmst476
+
+#Step1
+pima = read.csv("pima-indians-diabetes.csv", header = FALSE)
+plot(pima[,8], pima[,2], main = "Plasma glucose concentration on age", xlab = "Age", ylab = "Glucose")
+
+
