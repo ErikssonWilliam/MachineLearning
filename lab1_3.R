3707bc9f8464ae69e85616a4f4efdfeb25507873
William Eriksson <wiler441@student.liu.se>
2024-11-19 14:10:50 +0100

    modified 3-1. Solved 3-2 and 3-3

--- lab1_3.R
+++ lab1_3.R
@@ -1,6 +1,57 @@
 #marha614, wiler441, emmst476
 
+
 #Step1
 pima = read.csv("pima-indians-diabetes.csv", header = FALSE)
-plot(pima[,8], pima[,2], main = "Plasma glucose concentration on age", xlab = "Age", ylab = "Glucose",  col = ifelse(pima[, 9] == 1, "green", "red"),  # Color by diabetes levels
+
+glucose_conc = pima[,2]
+age = pima[,8]
+diabetes = pima[,9]
+
+plot(age, glucose_conc, main = "Plasma glucose concentration on age", xlab = "Age", ylab = "Glucose",  col = ifelse(diabetes == 1, "green", "red"),  # Color by diabetes levels
      pch = 19)
+#Step2
+
+library(dplyr)
+train=data.frame(glucose_conc, age, diabetes) #Selects the columns glucose concentration, age and diabetes
+
+m1 = glm(diabetes~age+glucose_conc, family = "binomial", data = train) #Train the logistic regression model
+summary(m1)
+
+#Probablistic equation see page 25 of lecuture 1 d
+
+coefficients = m1$coefficients
+logit = coefficients["(Intercept)"] + coefficients["age"]*train$age + coefficients["glucose_conc"]*train$glucose_conc
+prob_eq = 1/(1+ exp(-(logit))) #Logistic regression
+head(prob_eq) 
+
+#Prediction
+
+prob = predict(m1, type = "response") #Specify that the prediction returns the predicted probabilities
+pred = ifelse(prob > 0.5, "diabetes", "no diabetes")
+train_confusion_matrix=table(train$diabetes, pred)
+train_confusion_matrix
+
+mc_error = sum(diag(train_confusion_matrix)) / length(pred) #The incorrect classifications are in the diagonal of the confusion matrix. Length pred represents the total no of predictions made
+mc_error
+
+plot(train$age,train$glucose_conc, main="predicted plasma glucose concentration on age",
+     xlab="age", ylab="plasma glucose concentration", col = ifelse(pred == "diabetes", "green", "red"), pch=19)
+
+#Step 3
+#See page 23 of lecture 1d
+#Decision boundary is a curve that seprates different classes (Diabetes or no diabetes)
+#For binary model it would be when P(y=1|X) = 0.5 (Probabilistic equation)
+#Decision boundary for logistic regression is linear. (y = kx + m) Need to calculate the k and the m'
+#Decision boundary is where logit is 0 (prob_eq = 0.5)
+#=> glucose_conc = (-beta(0)/beta(2) = m value, -beta(1)/beta(2) = k)
+k = coefficients["age"]/-coefficients["glucose_conc"]
+m = coefficients["(Intercept)"]/-coefficients["glucose_conc"]
+
+#Equation
+sprintf("y = %fx + %f", k, m)
+
+#New Plot
+plot(train$age,train$glucose_conc, main="predicted plasma glucose concentration on age",
+     xlab="age", ylab="plasma glucose concentration", col = ifelse(pred == "diabetes", "green", "red"), pch=19)
+abline(a = m, b = k, col = "blue",  lwd = 2)
