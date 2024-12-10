df94fad418cdd2473acc2d4ce7e136963ecfe635
Martin Hallbäck <marha614@student.liu.se>
2024-12-10 12:12:42 +0100

    added plot for kernel 1, but plot for kernel 2 is failing

--- lab3_2.R
+++ lab3_2.R
@@ -17,7 +17,8 @@ date <- "2013-11-04" # The date to predict (up to the students)
 
 #times <- c("04:00:00", "06:00:00", ..., "24:00:00") THIS DIDNT WORK??
 times <- seq(from = 4, to = 24, by = 2)
-temp <- vector(length=length(times))
+temp_sum <- vector(length=length(times))
+temp_prod <- vector(length=length(times))
 
 # Students’ code here -----------------------------
 st_filtered = st[st$date <= date, ] #filter data to exlude future points
@@ -28,12 +29,21 @@ st_filtered = st[st$date <= date, ] #filter data to exlude future points
 distance = distHaversine(matrix(c(st_filtered$longitude, st_filtered$latitude), ncol = 2), c(b, a)) #calculates euclidean norm of x*-xi
 st_filtered$kernel_distance = exp(-(distance^2) / (h_distance^2))
 
+plot(distance, st_filtered$kernel_distance, type="o", 
+     main="Kernel Values vs. Distance (Spatial)",
+     xlab="Distance (meters)", ylab="Kernel Value",
+     col="blue", pch=16)
+
 #Kernel 2
 diff = as.numeric(difftime(date, st_filtered$date, units = "days"))
-#hist(diff, main="Distribution of Date Differences", xlab="Days Difference", breaks=50)
-#hist used to see distribution, to choose h
 
 st_filtered$kernel_date = exp(-(diff^2) / (h_date^2))
+
+plot(diff, st_filtered$kernel_date, type="o",  ##OBS THIS LOOKS WRONG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+     main="Kernel Values vs. Date Difference (Temporal)",
+     xlab="Days Difference", ylab="Kernel Value",
+     col="blue", pch=16)
+
 
 #Kernel 3
 hours = as.numeric(format(as.POSIXct(st_filtered$time, format="%H:%M:%S"), "%H"))
@@ -45,23 +55,33 @@ for (i in 1:length(times)) {
   st_filtered[column_name] =  exp(-(diff^2) / h_time^2)
 }
 
-#Sum Kernels
+#Sum Kernels and prod kernels
 for (i in 1:length(times)){
   column_name_time = paste("kernel_time_", times[i], sep="")
+  
   kernel_sum = st_filtered$kernel_distance + st_filtered$kernel_date + st_filtered[column_name_time]
-  temp[i] = sum(st_filtered$air_temperature * kernel_sum) / sum(kernel_sum)
+  temp_sum[i] = sum(st_filtered$air_temperature * kernel_sum) / sum(kernel_sum)
+  
+  kernel_prod = st_filtered$kernel_distance * st_filtered$kernel_date * st_filtered[column_name_time]
+  temp_prod[i] = sum(st_filtered$air_temperature * kernel_prod) / sum(kernel_prod)
 }
 
-#product kernels 
+y_min <- min(c(temp_sum, temp_prod)) - 1
+y_max <- max(c(temp_sum, temp_prod)) + 1  
 
-
-plot(temp, type="o", 
+plot(temp_sum, type="o", col="blue", 
      main=paste("Predicted Temperature for", date),
-     xlab="Time (Hours)", ylab="Temperature (°C)", xaxt="n")
+     xlab="Time (Hours)", ylab="Temperature (°C)", 
+     xaxt="n", ylim=c(y_min, y_max))  
 
 # Add custom x-axis labels (4, 6, 8, ..., 24)
 axis(1, at=1:length(times), labels=times)
 
+# Add the temperatures from the product of kernels (in a different color)
+lines(temp_prod, type="o", col="red")
+
+legend("topright", legend=c("Sum of Kernels", "Product of Kernels"), 
+       col=c("blue", "red"), pch=1)
 
 
 
